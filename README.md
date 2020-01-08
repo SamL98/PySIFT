bd055ef789c954c6566eed4ac926f6ff5ebaa7bc
Sam Lerner <lerner98@gmail.com>
2020-01-07 19:17:09 -0500

    changed argument handling and added example image

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This project contains an implementation of the SIFT keypoint extraction algorithm in Python. For a detailed explanation, visit the following blog post: https://medium.com/@lerner98/implementing-sift-in-python-36c619df7945.
 
+## Usage
+
+Pass in a filename for the `--input` argument and a prefix for the `--output` parameter. In addition to plotting the keypoints, `main.py` will save `results/<prefix>_kp_pyr.pkl` and `results/<prefix>_feats_pyr.pkl` files containing the computed features.
+
 ## Warning
 
 I do not maintain this repository. The code is provided as-is, so if you encounter an issue (which is likely) I would advise you to try and fix it yourself.
@@ -11,3 +15,7 @@ Also, if anyone wants to maintain this repository, leave an issue and I will gla
 ## Limitations 
 
 The code is unoptimized. This means that I took time to implement each step of SIFT as described in the paper as faithfully as I could but I did not do a second pass over the implementation for optimization. Therefore, on any reasonably-sized image, it should be fairly slow.
+
+In addition, there is currently a mistake in the keypoint scaling. When scaled up from the higher levels of the pyramid, they get off center. This can be seen in the following example:
+
+![example](example.png)
--- main.py
+++ main.py
@@ -1,49 +1,37 @@
 from skimage.io import imread
 from sift import SIFT
 
+import argparse
 import pickle
-from os.path import isfile
+import os
+from os.path import isdir
+
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-	num_img = 3
+	parser = argparse.ArgumentParser(description='PySIFT')
+	parser.add_argument('--input', type=str, dest='input_fname')
+	parser.add_argument('--output', type=str, dest='output_prefix', help='The prefix for the kp_pyr and feat_pyr files generated')
+	args = parser.parse_args()
 
-	kp_pyrs = []
-	ims = []
+	im = imread(args.input_fname)
 
-	for i in range(1, num_img+1):
-		im = imread('images/IMG_039%d.JPG' % i)
-		ims.append(im)
+	sift_detector = SIFT(im)
+	_ = sift_detector.get_features()
+	kp_pyr = sift_detector.kp_pyr
 
-		if isfile('results/kp_pyr%d.pkl' % i):
-			kp_pyrs.append(pickle.load(open('results/kp_pyr%d.pkl' % i, 'rb')))
-			continue
+	if not isdir('results'):
+		os.mkdir('results')
 
-		print('Performing SIFT on image %d' % i)
+	pickle.dump(sift_detector.kp_pyr, open('results/%s_kp_pyr.pkl' % args.output_prefix, 'wb'))
+	pickle.dump(sift_detector.feats, open('results/%s_feat_pyr.pkl' % args.output_prefix, 'wb'))
 
-		sift_detector = SIFT(im)
-		_ = sift_detector.get_features()
-		kp_pyrs.append(sift_detector.kp_pyr)
+	_, ax = plt.subplots(1, sift_detector.num_octave)
+	
+	for i in range(sift_detector.num_octave):
+		ax[i].imshow(im)
 
-		pickle.dump(sift_detector.kp_pyr, open('results/kp_pyr%d.pkl' % i, 'wb'))
-		pickle.dump(sift_detector.feats, open('results/feat_pyr%d.pkl' % i, 'wb'))
+		scaled_kps = kp_pyr[i] * (2**i)
+		ax[i].scatter(scaled_kps[:,0], scaled_kps[:,1], c='r', s=2.5)
 
-	import matplotlib.pyplot as plt
-
-	for i in range(len(kp_pyrs[0])):
-		_, ax = plt.subplots(1, 3)
-		ax[0].imshow(ims[0])
-
-		kps = kp_pyrs[0][i]*(2**i)
-		ax[0].scatter(kps[:,0], kps[:,1], c='b', s=2.5)
-
-		ax[1].imshow(ims[1])
-
-		kps = kp_pyrs[1][i]*(2**i)
-		ax[1].scatter(kps[:,0], kps[:,1], c='b', s=2.5)
-
-		ax[2].imshow(ims[2])
-
-		kps = kp_pyrs[2][i]*(2**i)
-		ax[2].scatter(kps[:,0], kps[:,1], c='b', s=2.5)
-
-		plt.show()
+	plt.show()
